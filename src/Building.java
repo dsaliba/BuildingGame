2f7a1dfdd81a4be670d5d1cdb8e43b37a3aa9779
dsaliba <dsaliba20@priorypanther.com>
2018-03-06 12:27:57 -0800

    edded stage

--- src/Building.java
+++ src/Building.java
@@ -2,11 +2,20 @@
 public abstract class Building {
 	
 	/**
+	 * The stage of evolution
+	 */
+	private int stage;
+	
+	
+	/**
 	 * This method returns a string showing what it does each day
 	 * @return String in format "(letter for building type) (Number)
 	 */
 	public abstract String newDay();
 	
-	
+	/**
+	 * This method is for updating the building type
+	 * @return Whether or not the building could be successfully upgraded
+	 */
 	public abstract boolean upgrade();
 }
