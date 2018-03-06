aa40ed528fffca10d175394be93e5f5aaeb11c47
dsaliba <dsaliba20@priorypanther.com>
2018-03-06 12:14:55 -0800

    Worked on bulding

--- src/Building.java
+++ src/Building.java
@@ -1,4 +1,12 @@
 
-public class Building {
-
+public abstract class Building {
+	
+	/**
+	 * This method returns a string showing what it does each day
+	 * @return String in format "(letter for building type) (Number)
+	 */
+	public abstract String newDay();
+	
+	
+	public abstract boolean upgrade();
 }
