01fbd3018eb806c2d09e129a99a191d3fa5b2817
dsaliba <dsaliba20@priorypanther.com>
2018-03-16 09:32:37 -0700

    idunno

--- src/UI/Buildmenu.java
+++ src/UI/Buildmenu.java
@@ -9,7 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
 public class Buildmenu extends JFrame {
-	private final String[] buildingLabels = { "a.png", "c.png", "d.png", "r.png", "s.png", "e.png" };
+	private final String[] buildingLabels = { "a.png", "c.png", "d.png", "r.png", "s.png"};
 	private Frame parent;
 	private JButton close;
 	private JButton upgrade;
