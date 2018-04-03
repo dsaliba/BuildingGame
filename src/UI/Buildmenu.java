b0fd1ff86e4799bf5496d24ad68c844b32840716
dsaliba <dsaliba20@priorypanther.com>
2018-04-03 00:30:05 -0700

    dont really knw

--- src/UI/Buildmenu.java
+++ src/UI/Buildmenu.java
@@ -3,6 +3,7 @@ package UI;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 
@@ -30,9 +31,9 @@ public class Buildmenu extends JFrame {
 	public Buildmenu(Frame parent) {
 
 		this.parent = parent;
-		setLayout(new GridLayout(2, 6));
+		setLayout(new GridBagLayout());
 		setResizable(true);
-		setBounds(500, 400, 1000, 360);
+		setBounds(500, 400, 1100, 360);
 
 		// setVisible(true);
 		createButtons();
@@ -52,67 +53,78 @@ public class Buildmenu extends JFrame {
 		add(closeButton);
 
 		for (int i = 0; i < buildingLabels.length; i++) {
-			
+			GridBagConstraints top = new GridBagConstraints();
+			top.gridheight = 2;
+			top.gridx = i;
+			top.gridy = 0;
 			bl = new JButton("");
 			bl.setIcon(new ImageIcon("Images\\" + buildingLabels[i] + ".png"));
 			bl.addActionListener(parent);
 			bl.setActionCommand("" + i);
 			bl.setFont(new Font("Monospaced", Font.BOLD, 40));
 			bl.setBackground(Color.WHITE);
-			add(bl);
+			add(bl, top);
 		}
 
 	}
 
 	public void createTextLabels() {
-
+		GridBagConstraints label = new GridBagConstraints();
+		label.gridheight = 0;
+		label.gridx = 0;
+		label.gridy = 2;
 		JLabel closeLabel = new JLabel("Close");
 		closeLabel.setFont(new Font("Monospaced", Font.BOLD, 40));
 		closeLabel.setVisible(true);
 		closeLabel.setBackground(Color.WHITE);
 		closeLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		closeLabel.setVerticalAlignment(SwingConstants.TOP);
-		add(closeLabel);
+		add(closeLabel, label);
 		
+		label.gridx++;
 		JLabel farmLabel = new JLabel("Farm");
 		farmLabel.setFont(new Font("Monospaced", Font.BOLD, 40));
 		farmLabel.setVisible(true);
 		farmLabel.setBackground(Color.WHITE);
 		farmLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		farmLabel.setVerticalAlignment(SwingConstants.TOP);
-		add(farmLabel);
+		add(farmLabel, label);
 		
+		label.gridx++;
 		JLabel merchantLabel = new JLabel("Shop");
 		merchantLabel.setFont(new Font("Monospaced", Font.BOLD, 40));
 		merchantLabel.setVisible(true);
 		merchantLabel.setBackground(Color.WHITE);
 		merchantLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		merchantLabel.setVerticalAlignment(SwingConstants.TOP);
-		add(merchantLabel);
+		add(merchantLabel, label);
 		
+		label.gridx++;
 		JLabel defenseLabel = new JLabel("Tower");
 		defenseLabel.setFont(new Font("Monospaced", Font.BOLD, 40));
 		defenseLabel.setVisible(true);
 		defenseLabel.setBackground(Color.WHITE);
 		defenseLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		defenseLabel.setVerticalAlignment(SwingConstants.TOP);
-		add(defenseLabel);
+		add(defenseLabel, label);
 		
+		label.gridx++;
 		JLabel recreationLabel = new JLabel("Tree");
 		recreationLabel.setFont(new Font("Monospaced", Font.BOLD, 40));
 		recreationLabel.setVisible(true);
 		recreationLabel.setBackground(Color.WHITE);
 		recreationLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		recreationLabel.setVerticalAlignment(SwingConstants.TOP);
-		add(recreationLabel);
+		add(recreationLabel, label);
 		
+		label.weightx++;
 		JLabel housingLabel = new JLabel("Tent");
 		housingLabel.setFont(new Font("Monospaced", Font.BOLD, 40));
 		housingLabel.setVisible(true);
 		housingLabel.setBackground(Color.WHITE);
 		housingLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		housingLabel.setVerticalAlignment(SwingConstants.TOP);
-		add(housingLabel);
+		add(housingLabel, label);
 		
 	}
 
