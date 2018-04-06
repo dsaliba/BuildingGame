67b4619a75182b6c2750327b43df975cfc993bfe
dsaliba <dsaliba20@priorypanther.com>
2018-04-05 22:52:51 -0700

    improved more orginization

--- src/UI/Upgrademenu.java
+++ src/UI/Upgrademenu.java
@@ -100,7 +100,7 @@ public class Upgrademenu extends JFrame{
 		add(removeLabel);
 		
 		upgradeLabel = new JLabel(upgradeText);
-		upgradeLabel.setFont(new Font("Monospaced", Font.BOLD, 20));
+		upgradeLabel.setFont(new Font("Monospaced", Font.BOLD, 40));
 		upgradeLabel.setVisible(true);
 		upgradeLabel.setBackground(Color.WHITE);
 		upgradeLabel.setHorizontalAlignment(SwingConstants.CENTER);
@@ -118,7 +118,7 @@ public class Upgrademenu extends JFrame{
 			upgrade.setForeground( Color.BLACK);
 			upgrade.setEnabled(true);
 		}
-		upgradeLabel.setText("Upgrade [" + building.getPrice() + "]");
+		upgradeLabel.setText("<HTML>Upgrade<br/>[" + building.getPrice() + "]</HTML>");
 	}
 	
 	public void toggleUpgrade(boolean showUpgrade) {
