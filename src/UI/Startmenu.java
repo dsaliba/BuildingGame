61545cfb98d515534673670de520818b62950191
Zlipher <diamondcrafter64@gmail.com>
2018-04-08 16:26:13 -0700

    added the start images

--- src/UI/Startmenu.java
+++ src/UI/Startmenu.java
@@ -57,29 +57,38 @@ public class Startmenu extends JFrame {
 	}
 
 	public void createButtons() {
-		startButton = new JButton("New Game"); // play button
-		startButton.setBounds(850, 400, 300, 100);
+		startButton = new JButton(""); // play button
+		startButton.setIcon(new ImageIcon("Images\\start.png"));
+		startButton.setBounds(850, 600, 300, 100);
 		startButton.setFont(new Font("Monospaced", Font.BOLD, 40));
 		startButton.setVisible(true);
 		startButton.addActionListener(parent);
 		startButton.setActionCommand("new game");
-		startButton.setBackground(Color.WHITE);
+		startButton.setOpaque(false);
+		startButton.setContentAreaFilled(false);
+		startButton.setBorderPainted(false);
 		
-		continueButton = new JButton("Continue"); // play button
-		continueButton.setBounds(850, 550, 300, 100);
+		continueButton = new JButton(""); // play button
+		continueButton.setIcon(new ImageIcon("Images\\continue.png"));
+		continueButton.setBounds(850, 700, 300, 100);
 		continueButton.setFont(new Font("Monospaced", Font.BOLD, 40));
 		continueButton.setVisible(true);
 		continueButton.addActionListener(parent);
 		continueButton.setActionCommand("continue");
-		continueButton.setBackground(Color.WHITE);
+		continueButton.setOpaque(false);
+		continueButton.setContentAreaFilled(false);
+		continueButton.setBorderPainted(false);
 
-		exitButton = new JButton("Exit"); // exit button on pause screen
-		exitButton.setBounds(850, 700, 300, 100);
+		exitButton = new JButton(""); // exit button on pause screen
+		exitButton.setIcon(new ImageIcon("Images\\exit.png"));
+		exitButton.setBounds(850, 800, 300, 100);
 		exitButton.setFont(new Font("Monospaced", Font.BOLD, 40));
 		exitButton.setVisible(true);
 		exitButton.addActionListener(parent); //----------------------------------------action listener stuff (v)
 		exitButton.setActionCommand("exit");
-		exitButton.setBackground(Color.WHITE);
+		exitButton.setOpaque(false);
+		exitButton.setContentAreaFilled(false);
+		exitButton.setBorderPainted(false);
 
 	}
 }
