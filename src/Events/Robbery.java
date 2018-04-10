ee495739930363bccdc7d3ce77334c1485dddf6a
dsaliba <dsaliba20@priorypanther.com>
2018-04-09 23:46:37 -0700

    change to robbery

--- src/Events/Robbery.java
+++ src/Events/Robbery.java
@@ -13,7 +13,7 @@ public class Robbery extends Event{
 	
 	@Override
 	public String execute(Stats stats) {
-		int num  = (int) (Stats.coins * ((Math.random() * 50) + 1));
+		int num  = (int) (Stats.coins * ((Math.random()/10)));
 		stats.coins -= num;
 		return "You have been robbed for " + num + " coins.";
 	}
