11b6f81d4dc6c0839762b93d9574c0e947b0572b
Fightersbane <lmsouter@gmail.com>
2018-04-12 10:44:03 -0700

    AAAAAAAAAAAA

--- src/Events/Prophet.java
+++ src/Events/Prophet.java
@@ -1,5 +1,26 @@
 package Events;
 
-public class Prophet {
+import Game.Stats;
+
+public class Prophet extends Event{
+
+	public Prophet() {
+		super();
+		probability = 2;
+		dayAvailable = 20;
+		buffer = -1;
+	}
+	
+	@Override
+	public String execute(Stats stats) {
+		Stats.happiness = 100;
+		Stats.coins += Stats.population * 100;
+		return "Jesus Christ has arrived! Great fortune will come!";
+	}
+
+	@Override
+	public boolean specialConditions(Stats stats) {
+		return true;
+	}
 
 }
