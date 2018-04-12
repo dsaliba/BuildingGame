27997ad12f9d1442c2779a7c2e47af2f692eebfb
Zlipher <diamondcrafter64@gmail.com>
2018-04-12 11:36:01 -0700

    extra credit options

--- src/Game/GameRunner.java
+++ src/Game/GameRunner.java
@@ -4,6 +4,23 @@ import java.io.IOException;
 import UI.Frame;
 import UI.Music;
 
+/*
+Michael Chang
+Dimitri Saliba
+Luke Souter
+
+Project 2 ExtraCredit
+
+For Extra Credit We Added:
+
+- Sound Effects
+- Button Animations
+- Histogram
+- Custom Events
+- Save Features
+
+*/
+
 public class GameRunner {
 	
 	public static void main(String[] args) throws IOException {
